02281b474e330efe4a868ac237fc65601c44bf3f
Rolando Guerra <rolandoguerra1991@gmail.com>
2021-09-21 00:21:01 -0400

    first commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitan America'];
+  heroeBorrado ?: string;
+
+  borrarHeroe() {
+    this.heroeBorrado = this.heroes.splice(0, 1).toString();
+  }
+}
