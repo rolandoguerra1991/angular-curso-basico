30fc0975030fa2a568ba2245ac0ffa6a20f61621
Rolando Guerra <rolandoguerra1991@gmail.com>
2021-09-29 20:42:30 -0400

    limpiqando codigo para desplegar

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.inderfaces';
-import { DbzService } from '../services/dbz.service';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
-
-  constructor() {}
 
   nuevo: Personaje = {
     nombre: 'Maestro Roshi',
--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.inderfaces';
 import { DbzService } from '../services/dbz.service';
 
